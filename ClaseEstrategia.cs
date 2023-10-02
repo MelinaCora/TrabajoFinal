f1f878dea4997031921763c2b29bdce5619de3f5
MelinaCora <144535945+MelinaCora@users.noreply.github.com>
2023-10-02 10:53:02 -0300

    Update ClaseEstrategia.cs

--- ClaseEstrategia.cs
+++ ClaseEstrategia.cs
@@ -25,24 +25,55 @@ namespace TP_obligatorio
 					
 		}
 		
-		public string Consulta1()//usar calcular distancia
+		public string Consulta1(ArbolGeneral<Planeta> arbol, Planeta botPlaneta, Planeta planeta)//en el progra deberia devolver mensaje adecuado
 		{
+    		List<string> camino = ObtenerCamino(arbol, botPlaneta, planeta);
+    		return camino;
 		}
 		
-		public string Consulta2()//usar ObtenerDescendientes
+		public string Consulta2(ArbolGeneral<Planeta> arbol)//usar ObtenerDescendientes
 		{
+			List<string> planetasDescendientes = ObtenerDescendientes(arbol, BotPlaneta);
+        		return "Planetas en los descendientes del nodo del Bot: " + string.Join(", ", planetasDescendientes);
 		}
 		
 		public string Consulta3()
 		{
 		}
 		
-		private List<string> ObtenerDescendientes() //devuelve una lista con los hijos de determinado planeta
+		private List<string> ObtenerDescendientes(arbol, Botplaneta) //devuelve una lista con los hijos de determinado planeta
 		{
+			List<Planeta> planetasEnPreorden = new List<Planeta>();
+    			RecorridoPreorden(arbol, planetaBot, planetasEnPreorden);
+
+    			// Obtén los nombres de los planetas en preorden
+    			List<string> nombresPlanetasEnPreorden = planetasEnPreorden.Select(p => p.Nombre).ToList();
+
+    			// Convierte la lista de nombres en un string separado por comas
+    			string listaPlanetasEnPreorden = string.Join(", ", nombresPlanetasEnPreorden);
+
+    			return listaPlanetasEnPreorden;
 		}
 		
-		private int CalcularDistancia()//calcular la distancia entre dos planetas
+		private List<string> ObtenerCamino(ArbolGeneral<Planeta> arbol, Planeta botPlaneta, Planeta planetaActual)
 		{
+    		if (planetaActual == null)
+        	return null;
+
+    		if (planetaActual.Equals(botPlaneta))
+        	return new List<string> { planetaActual.nombre };  // Encontramos el planeta, retornamos una lista con su nombre
+
+    		foreach (var hijo in arbol.ObtenerHijos(planetaActual))
+    		{
+        		var resultadoHijo = ObtenerCamino(arbol, botPlaneta, hijo.Dato);
+        		if (resultadoHijo != null)
+        		{
+            			resultadoHijo.Insert(0, planetaActual.nombre);  // Agregamos el nombre del planeta actual al camino
+            			return resultadoHijo;
+        		}
+   			}
+
+    			return null;
 		}
 		
 	}
