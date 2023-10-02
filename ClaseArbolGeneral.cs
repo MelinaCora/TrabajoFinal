64e7c65c39271efa60b8af9187b86369694178f1
MelinaCora <144535945+MelinaCora@users.noreply.github.com>
2023-10-02 11:38:58 -0300

    Update ClaseArbolGeneral.cs

--- ClaseArbolGeneral.cs
+++ ClaseArbolGeneral.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 
 namespace TP_obligatorio
 {
-    public class ArbolGeneral<T>
-    {
-        // Atributos
-        private T Dato { get; set; }
-        private List<ArbolGeneral<T>> Hijos { get; set; }
+	public class ArbolGeneral<T>
+   	 {
+        	// Atributos
+        	private T Dato { get; set; }
+        	private List<ArbolGeneral<T>> Hijos { get; set; }
 
-        // Constructor
-        public ArbolGeneral(T dato)
-        {
-            Dato = dato;
-            Hijos = new List<ArbolGeneral<T>>();
-        }
-        private void RecorridoPreorden(ArbolGeneral<T> nodo)
-	    {
-    	    if (nodo != null)
-    	    {
+        	// Constructor
+        	public ArbolGeneral(T dato)
+        	{
+            	Dato = dato;
+            	Hijos = new List<ArbolGeneral<T>>();
+        	}
+    	public void RecorridoPreorden()
+	{
+    		RecorridoPreorden(this);
+    	}
+     	private void RecorridoPreorden(ArbolGeneral<T> nodo)
+	{
+    	    	if (nodo != null)
+    		{
         	    Console.WriteLine(nodo.Dato);  // Visitar el nodo actual
 
         	    foreach (var hijo in nodo.Hijos)
         	    {
-            	    RecorridoPreorden(hijo);  // Recorrer cada hijo en profundidad
+            	    	RecorridoPreorden(hijo);  // Recorrer cada hijo en profundidad
         	    }
-    	    }
+    	    	}
         }
 }
