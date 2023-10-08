94b3b114d9234d22fd7019d26295b7b698b3de3e
MelinaCora <144535945+MelinaCora@users.noreply.github.com>
2023-10-08 18:10:19 -0300

    Update ClaseCola.cs

--- ClaseCola.cs
+++ ClaseCola.cs
@@ -6,37 +6,37 @@ namespace TP_obligatorio
 	public class Cola<T>
 	{
 		//Atributo
-		private Queue<T> queue;
+		private Cola<T> queue;
 		
 		//constructor
 		public Cola()
 		{
-			queue = new Queue<T>();
+			queue = new Cola<T>();
 		}
 		
 		//metodos
 		public void Encolar(T item)
-    	{
-        	queue.Encolar(item);
-    	}
+    		{
+        		queue.Encolar(item);
+    		}
 
-    	public T Desencolar()
-    	{
-        	if (queue.Count == 0)
-        	{
-            	throw new InvalidOperationException("La cola está vacía.");
-        	}
-        	return queue.Desencolar();
-    	}
+    		public T Desencolar()
+    		{
+    			if (queue.Contar() == 0)
+        		{
+            			Console.Writeline("La cola esta vacía");
+        		}
+        		return queue.Desencolar();
+    		}
 
-    	public int Contar()
-    	{
-        	return queue.Count;
-    	}
+    		public int Contar()
+    		{
+        		return queue.Contar();
+    		}
 
-    	public bool EstaVacia()
-    	{
-        	return queue.Count == 0;
-    	}
+    		public bool EstaVacia()
+    		{
+    			return queue.Contar() == 0;
+    		}
 	}
 }
