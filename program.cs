4cbf218e772e9109c1a7e201e8d87470449deabb
MelinaCora <144535945+MelinaCora@users.noreply.github.com>
2023-10-09 12:40:04 -0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -8,18 +8,46 @@ namespace TP_obligatorio
 		public static void Main(string[] args)
 		{
 			// Crear planetas para la IA
-            Planeta planetaIA1 = new Planeta("Azul", "IA1", 1000, 500, 10);
-            Planeta planetaIA2 = new Planeta("Azul", "IA2", 1500, 700, 15);
+            		Planeta planetaIA1 = new Planeta("Azul", "IA1", 1000, 300, 10);
+            		Planeta planetaIA2 = new Planeta("Azul", "IA2", 1500, 220, 15);
+            		Planeta planetaIA3 = new Planeta("Azul", "IA3", 1325, 300, 12);
+           		Planeta planetaIA4 = new Planeta("Azul", "IA4", 1100, 500, 18);
+            		Planeta planetaIA5 = new Planeta("Azul", "IA5", 990, 700, 22);
 
-            // Crear el árbol para la IA
-            ArbolGeneral<Planeta> arbolIA = new ArbolGeneral<Planeta>(planetaIA1, new List<Planeta> { planetaIA2 });
+           		// Crear el árbol para la IA
+            		ArbolGeneral<Planeta> arbolIA = new ArbolGeneral<Planeta>(planetaIA1, new List<Planeta> { planetaIA2 });
+            		arbolIA.AgregarHijo(planetaIA3);
+            		arbolIA.AgregarHijo(null)
+            		arbolIA.AgregarHijo(planetaIA4);
+            		arbolIA.AgregarHijo(planetaIA5);
+				
+				
+            		// Crear planetas para el jugador
+            		Planeta planetaJugador1 = new Planeta("Rojo", "Jugador1", 1200, 300, 12);
+           		Planeta planetaJugador2 = new Planeta("Rojo", "Jugador2", 1800, 220, 18);
+            		Planeta planetaJugador3 = new Planeta("Rojo", "Jugador3", 1000, 300, 10);
+            		Planeta planetaJugador4 = new Planeta("Rojo", "Jugador4", 1350, 500, 15);
+            		Planeta planetaJugador5 = new Planeta("Rojo", "Jugador5", 1440,700 , 12);
 
-            // Crear planetas para el jugador
-            Planeta planetaJugador1 = new Planeta("Rojo", "Jugador1", 1200, 600, 12);
-            Planeta planetaJugador2 = new Planeta("Rojo", "Jugador2", 1800, 800, 18);
-
-            // Crear el árbol para el jugador
-            ArbolGeneral<Planeta> arbolJugador = new ArbolGeneral<Planeta>(planetaJugador1, new List<Planeta> { planetaJugador2 });
+            		// Crear el árbol para el jugador
+           		ArbolGeneral<Planeta> arbolJugador = new ArbolGeneral<Planeta>(planetaJugador1, new List<Planeta> { planetaJugador2 });
+            		arbolJugador.AgregarHijo(planetaJugador3);
+            		arbolJugador.AgregarHijo(null);
+            		arbolJugador.AgregarHijo(planetaJugador4);
+            		arbolJugador.AgregarHijo(planetaJugador5);
+            
+            		Console.Write (arbolJugador.RecorridoPreorden());
+            		Console.Write (arbolIA.RecorridoPreorden());
+            
+            		//consulta 1 para planeta de la IA
+            		//consulta 2 para planeta de la IA
+            		//consulta 3 para planeta de la IA
+            		//calcular moviemiento para planeta de la IA
+            
+            		//consulta 1 para planeta del jugador
+            		//consulta 2 para planeta del jugador
+            		//consulta 3 para planeta del jugador
+            		//calcular moviemiento para planetel jugado
         	
 		}
 	}
