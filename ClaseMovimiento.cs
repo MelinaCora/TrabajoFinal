3bcbed8a4c384ff9d0e6bca53fce2e1207ae8106
MelinaCora <144535945+MelinaCora@users.noreply.github.com>
2023-10-30 11:02:25 -0300

    Update ClaseMovimiento.cs

--- ClaseMovimiento.cs
+++ ClaseMovimiento.cs
@@ -11,39 +11,49 @@ namespace TP_obligatorio
 		private Planeta planetaOrigen {get; set;}
 		private Plaeta planetaDestino {get;set;}
 		private int turno {get;set;}
+		public Flota Flota {get; set;}
 		
 		//constructor
-		public Movimiento(Planeta origen, Planeta destino, ArbolGeneral<Planeta> arbol)
+		public Movimiento(Planeta origen, Planeta destino, ArbolGeneral<Planeta> arbol, Flota flota)
 		{
 			Planeta origen=planetaOrigen;
 			Planeta destino= planetaDestino;
+			flota=Flota;
 			
 		}
 
 		public void RealizarMovimiento(Planeta botPlaneta,Planeta planetaObjetivo,ArbolGeneral<Planeta> arbol)
 		{
 			int turnox= turno.ObtenerTurno();
+			Planeta planetaActual = botPlaneta;
 			//obtener camino desde el planetaIA hasta el destino y realizar el movimiento solo si existe un camino directo
 			Planeta planetaObjetivo=planetaDestino.ObtenerPlanetaD()
 			List<Planeta> caminox = ObtenerCamino(arbol, planetaObjetivo);
-			while (turnox>0){
-			if (caminox != null)
-			{
-				//recorro cada nodo del camino obtenido y evaluo si es del jugador. en casos contrarios solo avanzo y disminuyo el turno			
-				foreach (Planeta nodo in caminox){
-					if (nodo.EsPlanetadelJugador()){
-						int FlotasBot=botPlaneta.ObtenerFlotas();
-						int FlotasObjetivo=planetaObjetivo.ObtenerFlotas();
-						botPlaneta.AgregarFlotas(FlotasObjetivo);
-						planetaObjetivo.DisminuirFlotas(FlotasObjetivo);
-						arbolJugador.EliminarHijo(planetaObjetivo);
-					}else{
-						turno--;
-					}
-				}
+			while (turnox > 0 && camino != null && camino.Count > 1){
+				// Avanzar al siguiente planeta en el camino
+        			Planeta siguientePlaneta = camino[1];
+				if (siguientePlaneta.EsPlanetadelJugador())
+				{
+					// Realizar interacción con el planeta objetivo (cálculo de flotas, eliminación, etc.)
+            				int flotasBot = botPlaneta.ObtenerFlotas();
+            				int flotasObjetivo = siguientePlaneta.ObtenerFlotas();
+            				botPlaneta.AgregarFlotas(flotasObjetivo);
+            				siguientePlaneta.DisminuirFlotas(flotasObjetivo);
+
+            				// Actualizar el árbol de juego
+           			 	arbol.EliminarHijo(siguientePlaneta);
 			
-			}
-		}
+				}
+				// Mover la flota de naves al siguiente planeta
+        			planetaActual.Flota = null;
+        			siguientePlaneta.Flota = botPlaneta.Flota;
+        			planetaActual = siguientePlaneta;
+
+        			// Reducir el turno y actualizar el camino
+        			turnox--;
+        			camino.RemoveAt(0);
+				}
+			}	
 		}
 		
 		public Planeta ObtenerPlanetaD()
