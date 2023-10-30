b3313374875dcc519b25b691c3c7bf141058845d
MelinaCora <144535945+MelinaCora@users.noreply.github.com>
2023-10-30 15:48:33 -0300

    Update ClaseMovimiento.cs

--- ClaseMovimiento.cs
+++ ClaseMovimiento.cs
@@ -26,31 +26,42 @@ namespace TP_obligatorio
 		{
 			int turnox=ObtenerTurno();
 			Planeta planetaActual = botPlaneta;
-			//obtener camino desde el planetaIA hasta el destino y realizar el movimiento solo si existe un camino directo
 			
-			List<Planeta> caminox =ObtenerCamino(arbol, planetaObjetivo);
-			while (turnox > 0 && caminox != null && caminox.Count > 1){
+			//primero realizo la busqueda del camino desde la IA hasta la raiz			
+			List<Planeta> caminox =ObtenerCamino(arbol, botPlaneta);			
+			// otra lista para el camino del destino hasta la raiz			
+			List<Planeta> caminoy = ObtenerCamino(arbol, planetaObjetivo);
+			//combinamos las listas para generar un solo camino			
+			List<Planeta> caminoz = new List<Planeta>(caminox);
+			caminoz.AddRange(caminoy);			
+			//obtengo las posiciones de ambos en el camino			
+			int indiceBot = caminoz.IndexOf(botPlaneta);
+			int indiceObjetivo = caminoz.IndexOf(planetaObjetivo);
+			//defino el nuevo camino desde el bot hasta 
+			List<Planeta> caminoFinal = caminoz.GetRange(indiceBot, indiceObjetivo - indiceBot + 1);
+						
+			while (turnox > 0 && caminoFinal != null && caminoFinal.Count > 1){
 				// Avanzar al siguiente planeta en el camino
-        			Planeta siguientePlaneta = caminox[1];
+        			Planeta siguientePlaneta = caminoFinal[1];
 				if (siguientePlaneta.EsPlanetadelJugador())
 				{
 					// Realizar interacción con el planeta objetivo 
-					planetaActual+=siguientePlaneta.naves();
+					planetaActual.AgregarNaves(siguientePlaneta.naves());
 					siguientePlaneta.naves()=0;
 					siguientePlaneta.ObtenerPropiedad()="Azul";
 			
 				}
 				if (siguientePlaneta.EsPlanetadelaIA()){
-					planetaActual.naves()+=siguientePlaneta.naves();
+					planetaActual.AgregarNaves(siguientePlaneta.naves());
 				}
 				// Mover la flota de naves al siguiente planeta
 				planetaActual.ObtenerFlota() = null;
-				siguientePlaneta.ObtenerFlota() = botPlaneta.ObtenerFlota();
+				siguientePlaneta.ObtenerFlota() = Flota;
         			planetaActual = siguientePlaneta;
 
-        		// Reducir el turno y actualizar el camino
-        		turnox--;
-        		caminox.RemoveAt(0);
+        			// Reducir el turno y actualizar el camino
+        			turnox--;
+        			caminoFinal.RemoveAt(0);
 			}
 		}			
 		public Planeta ObtenerPlanetaD()
