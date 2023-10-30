803058aee24d0017c268d1e3e3473e1eb9c3686e
MelinaCora <144535945+MelinaCora@users.noreply.github.com>
2023-10-30 10:58:19 -0300

    Update ClaseFlota.cs

--- ClaseFlota.cs
+++ ClaseFlota.cs
@@ -5,8 +5,37 @@ namespace TP_obligatorio
 	
 	public class Flota
 	{
-		public Flota()
+		//atributos
+		private string nombreFlota;
+		private int velocidad;
+		private int cantidadNaves;
+		
+		public Flota(string NombreFlota,int Velocidad,int CantidadNaves)
 		{
+			NombreFlota=nombreFlota;
+			Velocidad= velocidad;
+			CantidadNaves= cantidadNaves;
+		}
+
+		public string ObtenerNombreFlota(){
+			return nombreFlota;
+		}
+		public int ObtenerVelocidad(){
+			return velocidad;
+		}
+		public int ObtenerCantidadNaves(){
+			return cantidadNaves;
+		}
+		
+
+		public void AgregarFlotas(int FlotasParaAgregar) 
+		{
+			Flotas+=FlotasParaAgregar;
+		}
+		
+		public void DisminuirFlotas(int FlotasParaDisminuir)
+		{
+			Flotas-=FlotasParaDisminuir;
 		}
 	}
 }
