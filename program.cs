e2113d64dfcaa6ba72a3e350ebd8ad535d91b12b
MelinaCora <144535945+MelinaCora@users.noreply.github.com>
2023-10-30 16:50:33 -0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -39,34 +39,70 @@ namespace TP_obligatorio
 			//armo el arbol general
 			
             		ArbolGeneral <Planeta> Arbol1= new ArbolGeneral();
-            		Arbol1.AgregarHijo(planetaIA1); //raiz
+            		ArbolGeneral<Planeta> hijoPlaneta = new ArbolGeneral<Planeta>();
+			hijoPlaneta.Dato = planetaIA1; // Establecer el planeta como dato del hijo
+			Arbol1.AgregarHijo(hijoPlaneta); // Agregar el hijo al árbol principal
+			ArbolGeneral<Planeta> hijoPlaneta2 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlaneta3 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlaneta4 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlaneta5 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlaneta6 = new ArbolGeneral<Planeta>();
+            		hijoPlaneta2.Dato = planetaIA2;
+            		hijoPlaneta3.Dato = planetaIA3;
+            		hijoPlaneta4.Dato = planetaIA4;
+            		hijoPlaneta5.Dato = planetaIA5;
+            		hijoPlaneta6.Dato = planetaIA6;
+            		ArbolGeneral<Planeta> hijoPlanetaJ1 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaJ2 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaJ3 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaJ4 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaJ5 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaJ6 = new ArbolGeneral<Planeta>();
+            		hijoPlanetaJ1.Dato = planetaJugador1;
+            		hijoPlanetaJ2.Dato = planetaJugador2;
+            		hijoPlanetaJ3.Dato = planetaJugador3;
+            		hijoPlanetaJ4.Dato = planetaJugador4;
+            		hijoPlanetaJ5.Dato = planetaJugador5;
+            		hijoPlanetaJ6.Dato = planetaJugador6;
+            		ArbolGeneral<Planeta> hijoPlanetaN1 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaN2 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaN3 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaN4 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaN5 = new ArbolGeneral<Planeta>();
+            		ArbolGeneral<Planeta> hijoPlanetaN6 = new ArbolGeneral<Planeta>();
+            		hijoPlanetaN1.Dato = planetaNeutral1;
+            		hijoPlanetaN2.Dato = planetaNeutral2;
+            		hijoPlanetaN3.Dato = planetaNeutral3;
+            		hijoPlanetaN4.Dato = planetaNeutral4;
+            		hijoPlanetaN5.Dato = planetaNeutral5;
+            		hijoPlanetaN6.Dato = planetaNeutral6;
             		//nivel 1
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA1,planetaIA5); 
-           		Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA1,planetaJugador1);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA1,planetaJugador3);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA1,planetaIA3);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta,hijoPlaneta5); 
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta,hijoPlanetaJ1);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta,hijoPlanetaJ3);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta,hijoPlaneta3);
             		//nivel 2
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA5,planetaJugador5);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA5,planetaNeutral1);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaJugador1,planetaJugador6);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaJugador1,planetaIA2);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaJugador1,planetaNeutral2);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaJugador3,planetaJugador4);
-			Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA3,planetaJugador2);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA3,planetaNeutral6);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta5,hijoPlanetaJ5);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta5,hijoPlanetaN1);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlanetaJ1,hijoPlanetaJ6);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlanetaJ1,hijoPlaneta2);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlanetaJ1,hijoPlanetaN2);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlanetaJ3,hijoPlanetaJ4);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta3,hijoPlanetaJ2);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta3,hijoPlanetaN6);
             		//nivel 3
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaJugador5,planetaNeutral5);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaJugador5,planetaIA6);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaIA2,planetaNeutral4);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaJugador4,planetaNeutral3);
-            		Arbol1.AgregarHijoAunDeterminadoNodo(planetaJugador4,planetaIA4);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlanetaJ5,hijoPlanetaN5);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlanetaJ5,hijoPlaneta6);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlaneta2,hijoPlanetaN4);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlanetaJ4,hijoPlanetaN3);
+            		Arbol1.AgregarHijoAunDeterminadoNodo(hijoPlanetaJ4,hijoPlaneta4);
             
             		//ciclo de juego
             		List <Planeta> CaminoConveniente=estrategia1.CalcularMoviemiento(Arbol1,planetaIA1,planetaIA4);
             		estrategia1.Consulta1(Arbol1,planetaIA1,Arbol1.Dato);
            	 	estrategia1.Consulta2(Arbol1,planetaIA1);
             		estrategia1.Consulta3(Arbol1);
-               		Movimiento Juego1 = new Movimiento(planetaIA1,planetaIA4,Arbol1,FlotaIA);
+               		Movimiento Juego1 = new Movimiento(planetaIA1,planetaIA4,Arbol1,FlotaIA,4);
             		Juego1.RealizarMovimiento(CaminoConveniente,planetaIA1);
 
 			estrategia1.VerificarVictoria(Arbol1);
